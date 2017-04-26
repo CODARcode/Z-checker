dc0d4255091f1e194eebf88afc9037a52c401955
Sheng Di <disheng222@gmail.com>
2017-04-26 17:10:10 -0500

    add analyzeDataProperty.sh

---
+++ examples/analyzeDataProperty.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [[ $# < 2 ]]; then
+	echo Usage: $0 [data directory] [dimension sizes....] 
+	echo Example: $0 /home/shdi/CESM-testdata/x1800x3600 3600 1800
+	exit
+fi
+
+dataDir="$1"
+dim1=$2
+dim2=$3
+dim3=$4
+dim4=$5
+
+fileList=`cd "$dataDir";ls *.dat`
+
+for file in $fileList
+do
+	echo ./analyzeDataProperty zc.config "$dataDir/$file" $dim1 $dim2 $dim3 $dim4
+	./analyzeDataProperty zc.config "$dataDir/$file" $dim1 $dim2 $dim3 $dim4
+done
