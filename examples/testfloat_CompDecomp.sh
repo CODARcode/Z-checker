55618d1986eeef35abe0aff91dac9ba93fd21489
Sheng Di <disheng222@gmail.com>
2017-05-02 11:21:24 -0500

    new version 0.1.1

---
+++ examples/testfloat_CompDecomp.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cmdDir="/home/fti/SZ_C_version/sz-1.4.9-beta-normalsize/sz-1.4.9.2-beta/example"
+#rootDir="/run/media/fti/62D7F3A471F74BE2/NCAR-data"
+#rootDir="/run/media/fti/62D7F3A471F74BE2/CESM-bigdata"
+rootDir="/home/fti/SZ_C_version/CESM-testdata"
+absErrBound=1e-4
+
+targetDir="$rootDir"
+fileList=`cd "$targetDir";ls *.dat2`
+cd "$cmdDir"
+dimSizes="1800 3600" 
+for file in $fileList
+do
+	echo processing $file
+        echo testfloat_CompDecomp sz.config zc.config "sz($absErrBound)" "$file" $absErrBound "$targetDir/$file" $dimSizes
+        testfloat_CompDecomp sz.config zc.config "sz($absErrBound)" "$file" $absErrBound "$targetDir/$file" $dimSizes
+done
+
+echo "complete"
+
