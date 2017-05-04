1c58d6f1572e357989d075b2ff1ae09ca6139411
Sheng Di <disheng222@gmail.com>
2017-05-03 19:49:54 -0500

    remove *.pdf *.eps from template/figs

--- examples/generateReport.sh
+++ examples/generateReport.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-./generateGNUPlot zc.config 
+patch -p0 < ../../zc-patches/zc-compare.config.patch
+./generateGNUPlot zc.config "CESM-ATM-Tylor-Data"
+
+mkdir compareCompressors
+mv *.eps compareCompressors/
+
+mkdir compareCompressors/data
+mv *_*.txt compareCompressors/data
+
+mkdir compareCompressors/gnuplot_scripts
+mv *.p compareCompressors/gnuplot_scripts
+
+patch -r 
