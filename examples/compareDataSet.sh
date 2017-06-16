d416b690d0bf85a3da7cd539966f4eb173b2a2b9
dingwentao <ustc.dingwentao@gmail.com>
2017-06-15 20:44:46 -0400

    change compareDatSets.c and compareDataSet.sh to support -f and -d

---
+++ examples/compareDataSet.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+if [[ $# < 4 ]]; then
+	echo "Usage: compareDataSet.sh [data type (-f or -d)] [varName] [original data file] [user's decompressed data file] [dimension sizes...]"
+	exit
+fi
+
+datatype=$1
+varName=$2
+originaldataFile=$3
+decompresseddataFile=$4
+dim1=$5
+dim2=$6
+dim3=$7
+dim4=$8
+
+#run compressors
+cd ../../simple-lossy-compressors
+./compile-DeciIntrler.sh
+./compile-HfPrecCvter.sh
+
+
+if [ $datatype != "-f" ] && [ $datatype != "-d" ]; then 
+  echo "Error: datatype = $datatype . "
+  echo "Note: datatype can only be either -f or -d."
+  exit
+fi
+
+echo ./DeciIntrler 5 ${datatype} ${originaldataFile} ${dim1} $dim2 $dim3 $dim4
+./DeciIntrler 5 ${datatype} ${originaldataFile} ${dim1} $dim2 $dim3 $dim4
+
+echo ./HfPrecCvter ${datatype}  $originaldataFile $dim1 $dim2 $dim3 $dim4
+./HfPrecCvter ${datatype}  $originaldataFile $dim1 $dim2 $dim3 $dim4
+
+cd ../Z-checker/examples/
+./compareDataSets ${datatype} zc.config "hf-prec-cvter" $varName "${originaldataFile}" "${originaldataFile}.hf.out" $dim1 $dim2 $dim3 $dim4
+./compareDataSets ${datatype} zc.config "dec-interp-cmprsor" $varName "${originaldataFile}" "${originaldataFile}.intrpl.out" $dim1 $dim2 $dim3 $dim4
+./compareDataSets ${datatype} zc.config user-compressor $varName "${originaldataFile}" "$decompresseddataFile" $dim1 $dim2 $dim3 $dim4
--- examples/compareDataSets.c
+++ examples/compareDataSets.c
@@ -9,28 +9,28 @@ int main(int argc, char * argv[])
     char oriFilePath[640], decFilePath[640];
     char *cfgFile, *compressionCase, *varName;
 
-    if(argc < 7)
+    if(argc < 8)
     {
-        printf("Usage: compareDataSets [config_file] [compressionCase] [varName] [oriDataFilePath] [decDataFilePath] [dimension sizes...]\n");
-        printf("Example: compareDataSets zc.config SZ 8_8_128 testfloat_8_8_128.dat testfloat_8_8_128.dat.out 8 8 128\n");
+        printf("Usage: compareDataSets [dataType -f or -d] [config_file] [compressionCase] [varName] [oriDataFilePath] [decDataFilePath] [dimension sizes...]\n");
+        printf("Example: compareDataSets -f zc.config SZ 8_8_128 testfloat_8_8_128.dat testfloat_8_8_128.dat.out 8 8 128\n");
         exit(0);
     }
 
-    cfgFile=argv[1];
-    compressionCase=argv[2];
-    varName=argv[3];
-    sprintf(oriFilePath, "%s", argv[4]);
-    sprintf(decFilePath, "%s", argv[5]);
-    if(argc>=7)
-        r1 = atoi(argv[6]); //8
+    cfgFile=argv[2];
+    compressionCase=argv[3];
+    varName=argv[4];
+    sprintf(oriFilePath, "%s", argv[5]);
+    sprintf(decFilePath, "%s", argv[6]);
     if(argc>=8)
-        r2 = atoi(argv[7]); //8
+        r1 = atoi(argv[7]); //8
     if(argc>=9)
-        r3 = atoi(argv[8]); //128
+        r2 = atoi(argv[8]); //8
     if(argc>=10)
-        r4 = atoi(argv[9]);
+        r3 = atoi(argv[9]); //128
     if(argc>=11)
-        r5 = atoi(argv[10]);
+        r4 = atoi(argv[10]);
+    if(argc>=12)
+        r5 = atoi(argv[11]);
 
     printf("cfgFile=%s\n", cfgFile);
     printf("oriFilePath=%s\n", oriFilePath);
@@ -47,8 +47,23 @@ int main(int argc, char * argv[])
         exit(0);
     }	
 
-    ZC_CompareData* compareResult = ZC_compareData(varName, ZC_FLOAT, data1, data2, r5, r4, r3, r2, r1);
-	
+	ZC_CompareData* compareResult;
+
+	if (argv[1][1] == 'f')
+	{
+		compareResult = ZC_compareData(varName, ZC_FLOAT, data1, data2, r5, r4, r3, r2, r1);
+	}
+	else if (argv[1][1] == 'd')
+	{
+		compareResult = ZC_compareData(varName, ZC_DOUBLE, data1, data2, r5, r4, r3, r2, r1);
+	}
+	else
+	{
+		printf ("Wrong data type.\n");
+		printf ("Please use -f or -d to specify single or double data type.\n");
+		exit(0);
+	}
+
     ZC_printCompressionResult(compareResult);
     
     ZC_writeCompressionResult(compareResult, compressionCase, varName, "compressionResults");
