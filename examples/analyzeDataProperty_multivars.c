7be2299e1c1557e2b728343471e1d9418130b003
Sheng Di <sdi1@anl.gov>
2017-11-16 15:58:47 -0600

    add analyzeDataProperty_multivars.c

---
+++ examples/analyzeDataProperty_multivars.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <string.h>
+#include "zc.h"
+
+int main(int argc, char * argv[])
+{	
+    size_t r5=0,r4=0,r3=0,r2=0,r1=0;
+    char dataFile[640], outputFilePath[640], oriFilePath[640];
+	char multivarsConfFile[256];
+
+    if(argc < 3)
+    {
+        printf("Usage: analyzeDataProperty_multivars [zc_configFile] [multi_variable_data_file]\n");
+        printf("Example: analyzeDataProperty_multivars zc.config multivars.txt\n");
+        exit(0);
+    }
+
+	printf("argc=%d\n", argc);
+	ZC_Init(argv[1]);
+    sprintf(multivarsConfFile, "%s", argv[2]);
+
+	int nbVars;
+	ZC_CompareData** zcd = loadMultiVars(multivarsConfFile, &nbVars);
+	
+	int i = 0;
+	ZC_DataProperty* property = NULL;
+	for(i=0;i<nbVars;i++)
+	{
+		property = zcd[i]->property;
+		printf("var %d: %s, datatype=%d, r5=%zu, r4=%zu, r3=%zu, r2=%zu, r1=%zu\n", i, property->varName, property->dataType, property->r5, property->r4, property->r3, property->r2, property->r1);
+		if(property->dataType==ZC_FLOAT)
+		{
+			float* data = (float*)property->data;
+			printf("\t data=%f %f %f %f %f ....\n", data[0], data[1], data[2], data[3], data[4]);
+			free(data);
+		}
+		else
+		{
+			double* data = (double*)property->data;
+			printf("\t data=%f %f %f %f %f ....\n", data[0], data[1], data[2], data[3], data[4]);	
+			free(data);		
+		}
+	}
+	
+	for(i=0;i<nbVars;i++)
+		freeCompareResult(zcd[i]);	
+
+	free(zcd);
+	
+	ZC_Finalize();
+    printf("done\n");
+    
+    return 0;
+}
