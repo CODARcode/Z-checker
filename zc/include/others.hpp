4b2ef6d360f06e05f54254ce809deb54d768c6c9
Ali Murat Gok <agok@anl.gov>
2020-05-12 14:35:02 -0500

    Fixed out of bound bug in Sobolev Norm

--- zc/include/others.hpp
+++ zc/include/others.hpp
@@ -85,81 +85,85 @@ dT matrix<dT>::maxDiff(matrix &other){  //The maximum difference between two mat
 }
 
 template <typename dT>
-double matrix<dT>::sobolevNorm_s1_p2(matrix &other){  //The sobolevNorm between two matrices
+double matrix<dT>::sobolevNorm_s1_p2(matrix &rhs){  //The sobolevNorm between two matrices
   
-  if(size0!=other.size0){
+  if(size0!=rhs.size0){
     cout<<"matrix :: maxDiff : Size0's do not match."<<endl;
-    cout<<"  lhs.size0="<<size0<<"  rhs.size0="<<other.size0<<endl;
+    cout<<"  lhs.size0="<<size0<<"  rhs.size0="<<rhs.size0<<endl;
     assert(0);
   }
-  if(size1!=other.size1){
+  if(size1!=rhs.size1){
     cout<<"matrix :: maxDiff : Size1's do not match."<<endl;
-    cout<<"  lhs.size1="<<size1<<"  rhs.size1="<<other.size1<<endl;
+    cout<<"  lhs.size1="<<size1<<"  rhs.size1="<<rhs.size1<<endl;
     assert(0);
   }
-  if(size2!=other.size2){
+  if(size2!=rhs.size2){
     cout<<"matrix :: maxDiff : Size2's do not match."<<endl;
-    cout<<"  lhs.size2="<<size2<<"  rhs.size1="<<other.size2<<endl;
+    cout<<"  lhs.size2="<<size2<<"  rhs.size1="<<rhs.size2<<endl;
     assert(0);
   }
-  if(size3!=other.size3){
+  if(size3!=rhs.size3){
     cout<<"matrix :: maxDiff : Size3's do not match."<<endl;
-    cout<<"  lhs.size3="<<size3<<"  rhs.size1="<<other.size3<<endl;
+    cout<<"  lhs.size3="<<size3<<"  rhs.size1="<<rhs.size3<<endl;
     assert(0);
   }
   
   int i0;
-  int n=size0*size1*size2*size3;
+  int n;
   double result=0;
   
-  matrix<dT> odx,ody,odz,odt;
-  matrix<dT> ldx,ldy,ldz,ldt;
+  matrix<dT> ldx,ldy,ldz,ldt;  //Partial derivatives of left hand side
+  matrix<dT> rdx,rdy,rdz,rdt;  //Partial derivatives of right hand side
   
   if(nDim==1){
-    m_der_1d_v2(odx);
-    other.m_der_1d_v2(ldx);
+    m_der_1d_v2(ldx);
+    rhs.m_der_1d_v2(rdx);
+    n=ldx.size0*ldx.size1*ldx.size2*ldx.size3;
     for(i0=0;i0<n;i0++){
-      result+= (data[i0]-other.data[i0])*(data[i0]-other.data[i0]);
-      result+= (odx.data[i0]-odx.data[i0])*(odx.data[i0]-odx.data[i0]);
+      result+= (data[i0]-rhs.data[i0])*(data[i0]-rhs.data[i0]);
+      result+= (ldx.data[i0]-ldx.data[i0])*(ldx.data[i0]-ldx.data[i0]);
     }
   }else if(nDim==2){
-    m_der_2d_dim0_v2(odx);
-    m_der_2d_dim1_v2(ody);
-    other.m_der_2d_dim0_v2(ldx);
-    other.m_der_2d_dim1_v2(ldy);
+    m_der_2d_dim0_v2(ldx);
+    m_der_2d_dim1_v2(ldy);
+    rhs.m_der_2d_dim0_v2(rdx);
+    rhs.m_der_2d_dim1_v2(rdy);
+    n=ldx.size0*ldx.size1*ldx.size2*ldx.size3;
     for(i0=0;i0<n;i0++){
-      result+= (data[i0]-other.data[i0])*(data[i0]-other.data[i0]);
-      result+= (odx.data[i0]-odx.data[i0])*(odx.data[i0]-odx.data[i0]);
-      result+= (ody.data[i0]-ody.data[i0])*(ody.data[i0]-ody.data[i0]);
+      result+= (data[i0]-rhs.data[i0])*(data[i0]-rhs.data[i0]);
+      result+= (ldx.data[i0]-ldx.data[i0])*(ldx.data[i0]-ldx.data[i0]);
+      result+= (ldy.data[i0]-ldy.data[i0])*(ldy.data[i0]-ldy.data[i0]);
     }
   }else if(nDim==3){
-    m_der_3d_dim0_v2(odx);
-    m_der_3d_dim1_v2(ody);
-    m_der_3d_dim2_v2(odz);
-    other.m_der_3d_dim0_v2(ldx);
-    other.m_der_3d_dim1_v2(ldy);
-    other.m_der_3d_dim2_v2(ldz);
+    m_der_3d_dim0_v2(ldx);
+    m_der_3d_dim1_v2(ldy);
+    m_der_3d_dim2_v2(ldz);
+    rhs.m_der_3d_dim0_v2(rdx);
+    rhs.m_der_3d_dim1_v2(rdy);
+    rhs.m_der_3d_dim2_v2(rdz);
+    n=ldx.size0*ldx.size1*ldx.size2*ldx.size3;
     for(i0=0;i0<n;i0++){
-      result+= (data[i0]-other.data[i0])*(data[i0]-other.data[i0]);
-      result+= (odx.data[i0]-odx.data[i0])*(odx.data[i0]-odx.data[i0]);
-      result+= (ody.data[i0]-ody.data[i0])*(ody.data[i0]-ody.data[i0]);
-      result+= (odz.data[i0]-odz.data[i0])*(odz.data[i0]-odz.data[i0]);
+      result+= (data[i0]-rhs.data[i0])*(data[i0]-rhs.data[i0]);
+      result+= (ldx.data[i0]-ldx.data[i0])*(ldx.data[i0]-ldx.data[i0]);
+      result+= (ldy.data[i0]-ldy.data[i0])*(ldy.data[i0]-ldy.data[i0]);
+      result+= (ldz.data[i0]-ldz.data[i0])*(ldz.data[i0]-ldz.data[i0]);
     }
   }else if(nDim==4){
-    m_der_4d_dim0_v2(odx);
-    m_der_4d_dim1_v2(ody);
-    m_der_4d_dim2_v2(odz);
-    m_der_4d_dim3_v2(odt);
-    other.m_der_4d_dim0_v2(ldx);
-    other.m_der_4d_dim1_v2(ldy);
-    other.m_der_4d_dim2_v2(ldz);
-    other.m_der_4d_dim3_v2(ldt);
+    m_der_4d_dim0_v2(ldx);
+    m_der_4d_dim1_v2(ldy);
+    m_der_4d_dim2_v2(ldz);
+    m_der_4d_dim3_v2(ldt);
+    rhs.m_der_4d_dim0_v2(rdx);
+    rhs.m_der_4d_dim1_v2(rdy);
+    rhs.m_der_4d_dim2_v2(rdz);
+    rhs.m_der_4d_dim3_v2(rdt);
+    n=ldx.size0*ldx.size1*ldx.size2*ldx.size3;
     for(i0=0;i0<n;i0++){
-      result+= (data[i0]-other.data[i0])*(data[i0]-other.data[i0]);
-      result+= (odx.data[i0]-odx.data[i0])*(odx.data[i0]-odx.data[i0]);
-      result+= (ody.data[i0]-ody.data[i0])*(ody.data[i0]-ody.data[i0]);
-      result+= (odz.data[i0]-odz.data[i0])*(odz.data[i0]-odz.data[i0]);
-      result+= (odt.data[i0]-odt.data[i0])*(odt.data[i0]-odt.data[i0]);
+      result+= (data[i0]-rhs.data[i0])*(data[i0]-rhs.data[i0]);
+      result+= (ldx.data[i0]-ldx.data[i0])*(ldx.data[i0]-ldx.data[i0]);
+      result+= (ldy.data[i0]-ldy.data[i0])*(ldy.data[i0]-ldy.data[i0]);
+      result+= (ldz.data[i0]-ldz.data[i0])*(ldz.data[i0]-ldz.data[i0]);
+      result+= (ldt.data[i0]-ldt.data[i0])*(ldt.data[i0]-ldt.data[i0]);
     } 
   }
   
